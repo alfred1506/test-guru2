8b6eba947563bcdd48a68d770ac374db5df9c044
alfred1506 <nsk511119@mail.ru>
2022-11-13 14:47:35 +0500

    Task4

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,17 @@
 class Test < ApplicationRecord
+  has_many :questions
+  has_many :user_connection_tests
+  has_many :users, through: :user_connection_tests
+
+  belongs_to :category
+  belongs_to :author, class_name: 'User', foreing_key: :author_id
+
+  #def sort_names_by_category(name_category)
+  #  joins('JOIN categories ON categories.id = tests.category_id')
+  #  .where(categories: {title: name_category })
+  #end
+
   def sort_names_by_category(name_category)
-    joins('JOIN categories ON categories.id = tests.category_id')
-    .where(categories: {title: name_category })
+    joins(:category).where(categories: { title: name_category })
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ApplicationRecord
-  def test_list
-    Test.joins('JOIN user_connection_tests ON tests.id = user_connection_tests.tests_id')
+  has_many :user_connection_tests
+  has_many :tests, through: :user_connection_tests
+
+  #def test_list
+  #  Test.joins('JOIN user_connection_tests ON tests.id = user_connection_tests.tests_id')
+  #end
+
+  def test_list(value_level)
+    test.where(level: value_level)
+  end
 end
